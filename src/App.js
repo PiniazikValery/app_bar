c88839ddeca32f72b01fe11a15dd8523b6943a5c
ICX\valery.piniazik <valery.piniazik@itechart-group.com>
2019-08-02 17:05:04 +0300

    AMH - added basic header for mobile and desktop

--- src/App.js
+++ src/App.js
@@ -1,24 +1,39 @@
 import React from 'react';
-import logo from './logo.svg';
+import Responsive from 'react-responsive'
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import IconButton from '@material-ui/core/IconButton';
+import MenuIcon from '@material-ui/icons/Menu';
+import { Logo } from './AppStyledComponents';
 import './App.css';
+
+const Mobile = props => <Responsive {...props} maxWidth={414} />;
+const Default = props => <Responsive {...props} minWidth={415} />;
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Mobile>
+        <AppBar position="fixed">
+          <Toolbar>
+            <IconButton
+              edge="start"
+              color="inherit"
+              aria-label="open drawer"
+            >
+              <MenuIcon />
+            </IconButton>
+            <Logo src="/Logo.png" />
+          </Toolbar>
+        </AppBar>
+      </Mobile>
+      <Default>
+        <AppBar position="fixed">
+          <Toolbar>
+            <Logo src="/Logo.png" />
+          </Toolbar>
+        </AppBar>
+      </Default>
     </div>
   );
 }
---
+++ src/AppStyledComponents.js
@@ -0,0 +1,11 @@
+import styled from 'styled-components';
+
+export const Logo = styled.img`
+    margin-top:5.5px;
+    background-size:100% auto;
+    height:27px;
+    left:44px;
+    outline:0;
+    position:absolute;
+    width:211px;
+`
