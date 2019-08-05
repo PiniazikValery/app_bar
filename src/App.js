336ac35286e8199a7fc588916a2407a76693d6d9
ICX\valery.piniazik <valery.piniazik@itechart-group.com>
2019-08-05 17:46:21 +0300

    AMH - added search input element

--- src/App.js
+++ src/App.js
@@ -1,30 +1,60 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Responsive from 'react-responsive'
-import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
 import IconButton from '@material-ui/core/IconButton';
 import MenuIcon from '@material-ui/icons/Menu';
-import { Logo } from './AppStyledComponents';
+import { SearchIcon } from './icons';
+import {
+  Logo, PlaceholderForSearch, Grow,
+  ToolBar, SearchElement, SearchInput,
+  SearchButton, OpenSearchButton, SearchElementsWrapper,
+  AppBar, InputSearchIcon
+} from './AppStyledComponents';
 import './App.css';
 
 const Mobile = props => <Responsive {...props} maxWidth={414} />;
 const Default = props => <Responsive {...props} minWidth={415} />;
 
 function App() {
+  const [searchIsOpen, setOpenSearch] = useState(false);
+
+  const openSearchInput = () => setOpenSearch(true);
+
+  const closeSearchInput = () => setOpenSearch(false);
+
   return (
     <div className="App">
       <Mobile>
         <AppBar position="fixed">
-          <Toolbar>
-            <IconButton
-              edge="start"
-              color="inherit"
-              aria-label="open drawer"
-            >
-              <MenuIcon />
-            </IconButton>
-            <Logo src="/Logo.png" />
-          </Toolbar>
+          <ToolBar>
+            <PlaceholderForSearch>
+              <IconButton
+                edge="start"
+                color="inherit"
+                aria-label="open drawer"
+              >
+                <MenuIcon />
+              </IconButton>
+              <Logo src="/Logo_min.svg" />
+              <Grow />
+              <OpenSearchButton
+                onClick={openSearchInput}
+                edge="start"
+                color="inherit"
+                disableTouchRipple={true}
+                aria-label="open drawer"
+              >
+                <SearchIcon />
+              </OpenSearchButton>
+              <SearchElement open={searchIsOpen}>
+                <SearchElementsWrapper rightIndent={12}>
+                  <InputSearchIcon open={searchIsOpen} />
+                  <SearchInput type="search" />
+                  <SearchButton onClick={closeSearchInput}>НАЙТИ</SearchButton>
+                </SearchElementsWrapper>
+              </SearchElement>
+            </PlaceholderForSearch>
+          </ToolBar>
         </AppBar>
       </Mobile>
       <Default>
--- src/AppStyledComponents.js
+++ src/AppStyledComponents.js
@@ -1,11 +1,109 @@
 import styled from 'styled-components';
+import Toolbar from '@material-ui/core/Toolbar';
+import IconButton from '@material-ui/core/IconButton';
+import { SearchIcon } from './icons';
+import AppBar from '@material-ui/core/AppBar';
 
 export const Logo = styled.img`
-    margin-top:5.5px;
+    margin-top:6.5px;
     background-size:100% auto;
     height:27px;
-    left:44px;
     outline:0;
-    position:absolute;
-    width:211px;
+    position:relative;
 `
+
+export const PlaceholderForSearch = styled.div`
+    position: relative;
+    display: flex;
+    align-items: center;
+    width:100%;
+`
+
+export const ToolBar = styled(Toolbar)`
+    padding:0 !important;
+    padding-left: 12px !important;
+`
+
+export const Grow = styled.div`
+    flex-grow:1;
+`
+
+export const SearchElement = styled.div`
+    position: absolute;
+    transition: 0.2s;
+    right: 0;
+    display: flex;
+    align-items: center;
+    width:${(props) => {
+        if (props.open) {
+            return '100%;'
+        } else {
+            return '0%;'
+        }
+    }}
+    height:100%;
+`
+
+export const SearchInput = styled.input`
+    border-top-left-radius: 1.5px;
+    border-bottom-left-radius: 1.5px;
+    border: 0;
+    padding: 0;
+    padding-left: 25px;
+    border: 0;
+    width: 70%;
+    height: 70%;
+`
+
+export const SearchButton = styled.button`
+    border-top-right-radius: 1.5px;
+    border-bottom-right-radius: 1.5px;
+    color: white;
+    border: 0;
+    background-color: #ffc400;
+    text-align: center;
+    height: 70%;
+    width: 30%;
+`
+
+export const OpenSearchButton = styled(IconButton)`
+    &:hover {
+        background-color: transparent !important;
+    }
+`
+
+export const SearchElementsWrapper = styled.div`
+    overflow: hidden;
+    ${(props) => {
+        if (props.leftIndent) {
+            return `margin-left: ${props.leftIndent}px;`
+        }
+    }}
+    ${(props) => {
+        if (props.rightIndent) {
+            return `margin-right: ${props.rightIndent}px;`
+        }
+    }}
+    display: flex;
+    width: 100%;
+    height: 100%;
+    align-items: center;
+`
+
+const _AppBar = styled(AppBar)`
+    background-color: #1f77c7 !important;
+`
+
+export const InputSearchIcon = styled(SearchIcon)`
+    color: black;
+    position: absolute !important;
+    ${(props) => {
+        if (!props.open) {
+            return `
+            opacity: 0;
+        `
+        }
+    }}
+`
+
+export { _AppBar as AppBar };
